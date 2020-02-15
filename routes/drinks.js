e45c6174b6a67756b0662cd4904e63b94e22123c
leopolicastro <lpolicastro@pm.me>
2020-02-14 19:24:42 -0500

    connect express to api

---
+++ routes/drinks.js
@@ -0,0 +1,40 @@
+const { Router } = require('express');
+const router = Router();
+const axios = require('axios');
+
+router.get('/api/drinks', async (req, res, next) => {
+  const userChoice = req.query.drink;
+
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${userChoice}`;
+
+  axios
+    .get(url)
+    .then(function(response) {
+      const arr = response.data.drinks;
+      res.json(arr);
+    })
+    .catch(function(error) {
+      // handle error
+      res.json(error);
+    });
+  // .finally(function() {
+  //   // always executed
+  // });
+});
+
+router.get('/api/category', async (req, res, next) => {
+  const url = `https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list`;
+
+  axios
+    .get(url)
+    .then(function(response) {
+      const arr = response.data;
+      res.json(arr);
+    })
+    .catch(function(error) {
+      // handle error
+      res.json(error);
+    });
+});
+
+module.exports = router;
