afa10e15df850380a11cece24738014138f061e1
nklgraphics <natalie@nklgraphics.com>
2020-02-16 09:39:42 -0500

    update 02152020

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,29 @@
 import React from 'react';
+import {BrowserRouter, Switch, Route} from "react-router-dom"
+import Home from './pages/Home'
+import Navigation from './components/Navigation'
+import About from './pages/About'
+import About from './pages/About'
+import Catagories from './pages/Contacts'
+
 import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+      <Navigation/>
+      <Switch>
+        <Route exact path = "/" component={Home}/>
+        <Route exact path = "/about" component={About}/>
+        <Route exact path = "/catagories" component={Catagories}/>
+      </Switch>
+      </BrowserRouter>
+      
+    </div>
+  );
+}
+//created by Natalie Lozano 02.15.2020
 
 class App extends React.Component {
   state = { serverMessage: '' };
