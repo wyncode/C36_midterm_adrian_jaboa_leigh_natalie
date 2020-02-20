14168c348cdb6dfadf33c4ddbd556291545b639c
gadrian99 <adriangarcia1999@yahoo.com>
2020-02-19 20:28:51 -0500

    about/css

--- client/src/components/AboutUs.jsx
+++ client/src/components/AboutUs.jsx
@@ -8,35 +8,30 @@ import groupPhoto from '../photos/group.jpg'
 
 
 const AboutUs = () => {
-
-
-
-    return ( 
+    return (
         <>
-        <div className="hero-container" style={{display: 'flex'}}>
-            <div className="Hero1">
-                <img src={jaboa} alt="Jaboa"style={{height: '17rem', width: '14rem'}}/>
-            <p>Jaboa's favorite drink is Hennessey and Red Bull</p>
-            </div>
-            <div className="Hero2">
-            <img src={natalie} alt="Natalie" style={{height: '17rem', width: '14rem'}}/>
-            <p>Natalie's favorite drink is Moscow Mule</p>
-            </div>
-            <div className="Hero3">
-            <img src={adrian} alt="Adrian" style={{height: '17rem', width: '14rem'}}/>
-                <p>Adrian's favorite drink is Pina Coloda</p>
-            </div>
-            <div className="Hero4">
-            <img src={leigh} alt="leigh" style={{height: '17rem', width: '14rem'}}/>
-                <p>Leigh's favorite drink is Tanqueray and Sprite</p>  
-            </div>
+            <h1 className="hero-title">"On the eve of Valentines, this band of devoted developers came together to help bring more ways to help everyone drink responsibly. You're welcome" </h1>
 
-        
-    </div>    
-
-<h1 className="hero-title">"On the eve of Valentines, this band of devoted developers came together to help bring more ways to help everyone drink responsibly. You're welcome" </h1>
-</>
+            <div className="hero-container" style={{display: 'flex', justifyContent: 'space-evenly'}}>
+                <div className="hero-container_div">
+                    <img className="hero-container_img"src={jaboa} alt="Jaboa"style={{height: '17rem', width: '14rem'}}/>
+                    <p className="hero-container_text">Jaboa's favorite drink is Hennessey and Red Bull</p>
+                </div>
+                <div className="hero-container_div">
+                    <img className="hero-container_img" src={natalie} alt="Natalie" style={{height: '17rem', width: '14rem'}}/>
+                    <p className="hero-container_text">Natalie's favorite drink is Moscow Mule</p>
+                </div>
+                <div className="hero-container_div">
+                    <img className="hero-container_img" src={adrian} alt="Adrian" style={{height: '17rem', width: '14rem'}}/>
+                        <p className="hero-container_text">Adrian's favorite drink is Pina Coloda</p>
+                </div>
+                <div className="hero-container_div">
+                    <img className="hero-container_img" src={leigh} alt="leigh" style={{height: '17rem', width: '14rem'}}/>
+                    <p className="hero-container_text">Leigh's favorite drink is Tanqueray and Sprite</p>
+                </div>
+            </div>
+         </>
     )
 }
- 
+
 export default AboutUs;
